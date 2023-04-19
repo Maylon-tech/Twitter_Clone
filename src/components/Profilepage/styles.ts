183890fb0a22c62d00fccc638e97966b8fec8aab
Maylon-tech <minatokiroisen77@gmail.com>
2023-04-19 23:02:16 +0900

    building banner profile layout

--- src/components/Profilepage/styles.ts
+++ src/components/Profilepage/styles.ts
@@ -2,13 +2,49 @@ import styled, { css } from "styled-components";
 import { LocationOn, Cake } from '../../styles/icons'
 
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 100%;
+  overflow-y: auto;
+  
+  scrollbar-width: none; // Firefox
+  // Chrome
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
 
-export const Banner = styled.div``
+export const Banner = styled.div`
+  flex-shrink: 0; // Nao deixar diminuir o tamanho.
+  width: 100%;
+  height: min(33vw, 199px);
+  background-color: var(--twitter);
+  position: relative;
 
-export const Avatar = styled.div``
+`
 
-export const ProfileData = styled.div``
+export const Avatar = styled.div`
+  width: max(45px, min(135px, 22vw));
+  height: max(45px, min(135px, 22vw));
+
+  border: 3.75px solid var(--primary);
+  background: var(--gray);
+  border-radius: 50%;
+
+  position: absolute;
+  bottom: max(-60px, -10vw);
+  left: 15px;
+`
+
+export const ProfileData = styled.div`
+  padding: min(calc(10vw + 7px), 67px) 16px 0;
+
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+`
 
 export const EditButton = styled.div``
 
